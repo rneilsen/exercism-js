ff34055a723bcdcf570903b40c63ae90aed78d4a
rneilsen <8201743+rneilsen@users.noreply.github.com>
2022-02-09 17:34:13 +1100

    Complete lasagna exercise

--- lasagna/lasagna.js
+++ lasagna/lasagna.js
@@ -35,6 +35,7 @@
  */
 const PREPARATION_MINUTES_PER_LAYER = 2;
 
+export const EXPECTED_MINUTES_IN_OVEN = 40;
 /**
  * Determines the number of minutes the lasagna still needs to remain in the
  * oven to be properly prepared.
@@ -43,7 +44,7 @@ const PREPARATION_MINUTES_PER_LAYER = 2;
  * @returns {number} the number of minutes remaining
  */
 export function remainingMinutesInOven(actualMinutesInOven) {
-  throw new Error('Please implement the remainingMinutesInOven function');
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
 }
 
 /**
@@ -53,7 +54,7 @@ export function remainingMinutesInOven(actualMinutesInOven) {
  * @returns {number} the total preparation time
  */
 export function preparationTimeInMinutes(numberOfLayers) {
-  throw new Error('Please implement the preparationTimeInMinutes function');
+  return numberOfLayers * PREPARATION_MINUTES_PER_LAYER;
 }
 
 /**
@@ -65,5 +66,5 @@ export function preparationTimeInMinutes(numberOfLayers) {
  * @returns {number} the total working time
  */
 export function totalTimeInMinutes(numberOfLayers, actualMinutesInOven) {
-  throw new Error('Please implement the totalTimeInMinutes function');
+  return preparationTimeInMinutes(numberOfLayers) + actualMinutesInOven;
 }

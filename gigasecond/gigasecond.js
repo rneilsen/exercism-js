60946cd41c73f2f93f7e5fb41e17d301f18cf068
Richard Neilsen <RNeilsen@users.noreply.github.com>
2022-02-17 16:27:20 +1100

    Complete gigasecond exercise

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -3,6 +3,8 @@
 // convenience to get you started writing code faster.
 //
 
-export const gigasecond = () => {
-  throw new Error('Remove this statement and implement this function');
+export const gigasecond = (date) => {
+  let newDate = new Date(date);
+  newDate.setUTCSeconds(date.getUTCSeconds() + 10 ** 9);
+  return newDate;
 };

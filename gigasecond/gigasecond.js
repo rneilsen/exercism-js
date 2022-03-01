352d4acabbb73d3fd17c2af823f118ff702834b9
Richard Neilsen <RNeilsen@users.noreply.github.com>
2022-03-01 15:06:03 +1100

    Simplify to one-line function

--- gigasecond/gigasecond.js
+++ gigasecond/gigasecond.js
@@ -5,6 +5,4 @@
 
 const GIGASECOND_IN_MS = 10 ** 12;
 
-export const gigasecond = (date) => {
-  return new Date(date.getTime() + GIGASECOND_IN_MS);
-};
+export const gigasecond = (date) => new Date(date.getTime() + GIGASECOND_IN_MS);

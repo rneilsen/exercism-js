1e8bac36565366033c58d5a06a3d419712a41dac
Richard Neilsen <RNeilsen@users.noreply.github.com>
2022-03-04 15:50:52 +1100

    Implement countWords() function

--- word-count/word-count.js
+++ word-count/word-count.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const countWords = () => {
-  throw new Error('Remove this statement and implement this function');
+export const countWords = (input) => {
+  function splitIntoWords(word) {
+    const wordRegex = /([a-z]+'[a-z]|[a-z]+|[0-9]+)/g;
+    return word.toLowerCase().match(wordRegex);
+  }
+
+  let wordCounts = {};
+  const currentCount = (someWord) => wordCounts[someWord] ?? 0;
+
+  let words = splitIntoWords(input);
+  console.log(words);
+
+  for (let i = 0; i < words.length; i++) {
+    const word = words[i];
+    wordCounts[word] = currentCount(word) + 1;
+  }
+
+  return wordCounts;
 };

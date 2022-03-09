8e7353460e4e242185ef15fb94c51b6ecdf1c643
Richard Neilsen <RNeilsen@users.noreply.github.com>
2022-03-09 15:10:58 +1100

    Use faster algorithm for finding factors

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -1,13 +1,35 @@
+function getDivisors(number, divisor) {
+  if (number % divisor !== 0) {
+    return [];
+  }
+  let n = divisor;
+  let factors = [];
+  while (n < number) {
+    if (number % n === 0) factors.push(n);
+    n += divisor;
+  }
+  return factors;
+}
+
 export const classify = (number) => {
   if (number <= 0 || !Number.isInteger(number)) {
     throw "Classification is only possible for natural numbers.";
   }
-  let aliquotSum = 0;
-  for (let i = 0; i <= Math.floor(number / 2); i++) {
-    if (number % i === 0) {
-      aliquotSum += i;
-    }
+  if (number === 1) {
+    return "deficient"; /* have to handle 1 as special case */
   }
+
+  let factors = [1, ...getDivisors(number, 2), ...getDivisors(number, 3)];
+  for (let i = 6; i < number / 2 + 1; i += 6) {
+    // apart from 2 and 3, all primes are of the form (6k +/- 1)
+    factors = factors.concat(getDivisors(number, i - 1));
+    factors = factors.concat(getDivisors(number, i + 1));
+  }
+
+  const factorSet = new Set(factors);
+  let aliquotSum = 0;
+  for (let item of factorSet) aliquotSum += item;
+
   if (aliquotSum < number) {
     return "deficient";
   } else if (aliquotSum > number) {

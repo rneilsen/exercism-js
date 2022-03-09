40f5ffb27040e8336a587c58a37546d1b085c959
Richard Neilsen <RNeilsen@users.noreply.github.com>
2022-03-09 14:23:20 +1100

    Complete perfect-numbers exercise

--- perfect-numbers/perfect-numbers.js
+++ perfect-numbers/perfect-numbers.js
@@ -1,8 +1,18 @@
-//
-// This is only a SKELETON file for the 'Perfect Numbers' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const classify = () => {
-  throw new Error('Remove this statement and implement this function');
+export const classify = (number) => {
+  if (number <= 0) {
+    throw "Classification is only possible for natural numbers.";
+  }
+  let aliquotSum = 0;
+  for (let i = 0; i <= Math.floor(number / 2); i++) {
+    if (number % i === 0) {
+      aliquotSum += i;
+    }
+  }
+  if (aliquotSum < number) {
+    return "deficient";
+  } else if (aliquotSum > number) {
+    return "abundant";
+  } else {
+    return "perfect";
+  }
 };

64ea47a2c3dbc2b0e735126e1fff24571d23e0c3
Richard Neilsen <RNeilsen@users.noreply.github.com>
2022-03-10 14:25:17 +1100

    Complete lasagna-master exercise

--- lasagna-master/lasagna-master.js
+++ lasagna-master/lasagna-master.js
@@ -1,12 +1,40 @@
 /// <reference path="./global.d.ts" />
 // @ts-check
 
-/**
- * Implement the functions needed to solve the exercise here.
- * Do not forget to export them so they are available for the
- * tests. Here an example of the syntax as reminder:
- *
- * export function yourFunction(...) {
- *   ...
- * }
- */
+export function cookingStatus(timeRemaining) {
+  switch (timeRemaining) {
+    case undefined:
+      return "You forgot to set the timer.";
+    case 0:
+      return "Lasagna is done.";
+    default:
+      return "Not done, please wait.";
+  }
+}
+
+export function preparationTime(layers, avgTime) {
+  return layers.length * (avgTime ?? 2);
+}
+
+const arrayCount = (array, value) =>
+  array.filter((item) => item === value).length;
+
+export function quantities(layers) {
+  return {
+    noodles: 50 * arrayCount(layers, "noodles"),
+    sauce: 0.2 * arrayCount(layers, "sauce"),
+  };
+}
+
+export function addSecretIngredient(friendsList, myList) {
+  myList.push(friendsList.at(-1));
+}
+
+export function scaleRecipe(recipe, people) {
+  let newRecipe = {};
+  const scaleFactor = people / 2;
+  for (let ingredient in recipe) {
+    newRecipe[ingredient] = scaleFactor * recipe[ingredient];
+  }
+  return newRecipe;
+}
